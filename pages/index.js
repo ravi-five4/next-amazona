1038b3079b9e52ce7e6f56309eea63233b45949c
ravi.gandhi <ravi.gandhi@justdial.com>
2022-09-26 15:45:31 +0530

    product data render

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
 import { Layout } from "../components/Layout";
+import { Productitem } from "../components/Productitem";
+import data from "../utils/data";
 
 export default function Home() {
-  return <Layout>Home Page</Layout>;
+  return (
+    <Layout title="Home Page">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {data.products.map((product) => (
+          <Productitem product={product} key={product.slug}></Productitem>
+        ))}
+      </div>
+    </Layout>
+  );
 }
