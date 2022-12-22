92bf026ca799ac93439f7fa60b08a85a522beb14
ravi.gandhi <ravi.five4@gmail.com>
2022-12-22 18:33:58 +0530

    TC of  Login user and product add to cart

--- cypress/e2e/homepage/home.cy.js
+++ cypress/e2e/homepage/home.cy.js
@@ -1,6 +1,26 @@
-describe("My first test", () => {
-  it("Home Page", () => {
+describe("Next Justdial", () => {
+  it("Home Page", function () {
+    //visit home page
     cy.visit("http://localhost:3000/");
-    expect(true).to.equal(true);
+    // check scroll
+    cy.scrollTo("bottom", { easing: "swing" });
+    // select any product to add to cart
+  });
+  it("Add Product to Cart ", function () {
+    //visit home page
+    cy.visit("http://localhost:3000/");
+    // check scroll
+    cy.scrollTo("bottom", { easing: "swing" });
+    // select any product to add to cart like product at index 2
+    cy.get("button[type=button]").eq(2).click();
+    // select any product to add to cart like product at index 1
+    cy.get("button[type=button]").eq(1).click();
+    // check toast pop up whether it's coming or not.
+    cy.get(".Toastify", { timeout: 10000 }).should(
+      "have.text",
+      "Product added to the cart"
+    );
+    // since two items added to the cart
+    cy.get(".m-1").contains("2").should("have.text", "2");
   });
 });
--- cypress/e2e/user/user.cy.js
+++ cypress/e2e/user/user.cy.js
@@ -1,18 +1,17 @@
 const { data } = require("autoprefixer");
 
-describe("example to-do app", function () {
+describe("Next Justdial", function () {
   before(() => {
     cy.fixture("user").then(function (data) {
       this.data = data;
       // this means data reference
     });
     cy.visit("http://localhost:3000/");
-  });
-  it("Register New account", function () {
-    // all cookies clear
     cy.clearCookies();
     cy.clearAllSessionStorage();
-    // get log in  element to register
+  });
+  it("Register New account and Logout", function () {
+    // get log in  element to register new account
     cy.get(".p-2").contains("Login").click();
     // expected url should include login keyword
     cy.url().should("include", "login");
@@ -35,7 +34,30 @@ describe("example to-do app", function () {
     cy.get("#confirmPassword").type(this.data.confirmPassword);
     // cy.get('form').submit();
     cy.get("form").submit();
+    // verify url after submission of form
+    cy.url().should("eq", "http://localhost:3000/");
+    // click on user to pop up menu
+    cy.contains(this.data.name).click();
+    // log out user
+    cy.get(".dropdown-link").contains("Logout").click();
+  });
 
+  it("Login with valid credentials", function () {
+    // visit home page
+    cy.visit("http://localhost:3000/");
+    // find login keyword to login using valid credentials
+    cy.get(".p-2").contains("Login").click();
+    // expected url should include login keyword
+    cy.url().should("include", "login");
+    // url assertions
+    cy.url().should("eq", "http://localhost:3000/login");
+    // Input field Email
+    cy.get("#email").should("have.attr", "type", "email").type(this.data.email);
+    // input field password
+    cy.get("#password").type(this.data.password);
+    // cy.get('form').submit();
+    cy.get("form").submit();
+    // verify url after submission of form
     cy.url().should("eq", "http://localhost:3000/");
   });
 });
